92bec606cbf1d49b3d11fbabfd7cb0c147781fe9
Samonto Karmaker <samonto0726@gmail.com>
2025-04-30 19:13:09 +0600

    Refactor App component and add Todo component

---
+++ src/component/Todo.tsx
@@ -0,0 +1,11 @@
+export default function Todo() {
+    return (
+        <div className="Todo">
+            <h1>Todo</h1>
+            <p>
+                To get started, edit <code>src/component/Todo.tsx</code> and
+                save to reload.
+            </p>
+        </div>
+    );
+}
