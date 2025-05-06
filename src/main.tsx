0acb01ebcd091a3351818f23689ade01a6712c82
Samonto Karmaker <samonto0726@gmail.com>
2025-05-06 12:19:45 +0600

    feat: add Mock Service Worker setup and configuration

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { worker } from './api/browser.ts'
+
+if(import.meta.env.DEV) {
+  // Enable the mocking in development mode
+  worker.start({
+    onUnhandledRequest: 'bypass',
+  })
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
