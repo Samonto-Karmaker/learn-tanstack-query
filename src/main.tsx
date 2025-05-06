4c51ec5a61c7c831e0c2289148b08ddf7027b626
Samonto Karmaker <samonto0726@gmail.com>
2025-05-06 12:49:41 +0600

    feat: integrate QueryClientProvider for React Query setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { worker } from './api/browser.ts'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "./index.css"
+import App from "./App.tsx"
+import { worker } from "./api/browser.ts"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
-if(import.meta.env.DEV) {
-  // Enable the mocking in development mode
-  worker.start({
-    onUnhandledRequest: 'bypass',
-  })
+if (import.meta.env.DEV) {
+    // Enable the mocking in development mode
+    worker.start({
+        onUnhandledRequest: "bypass",
+    })
 }
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const queryClient = new QueryClient()
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
+    </StrictMode>
 )
